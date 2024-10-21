d592654f498d5673a0e878091370a774a3706f71
bagumamuhsinkarim <mosh60964@gmail.com>
2024-10-21 17:36:06 +0300

    collaborativeAlgo

---
+++ RecommendationAlgorithm/colaborativeAlgo/README.md
@@ -0,0 +1,25 @@
+Required imports when using the collobarative algrithm
+import pandas
+from sklearn.metrics.pairwise import cosine_similarity
+
+This is an example used of the collaboratie Algorithm
+
+if __name__ == "__main__":
+    # Sample user-item rating matrix
+    data = {
+        'User1': [5, 4, 0, 0, 3],
+        'User2': [4, 0, 0, 2, 4],
+        'User3': [0, 0, 5, 3, 0],
+        'User4': [0, 2, 4, 0, 0],
+        'User5': [1, 0, 0, 5, 4]
+    }
+    items = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5']
+    
+    # Instantiate recommender system
+    recommender = RecommenderSystem(data)
+    
+    # Example: get recommendations for User1
+    user_to_recommend = 'User1'
+    recommended_items = recommender.get_recommendations(user_to_recommend)
+    print(f"Recommended items for {user_to_recommend}:")
+    print(recommended_items)
--- RecommendationAlgorithm/colaborativeAlgo/collaborativeAgorithm.py
+++ RecommendationAlgorithm/colaborativeAlgo/collaborativeAgorithm.py
@@ -1,44 +1,27 @@
-import pandas as pd
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+class RecommenderSystem:
+    def __init__(self, ratings_data):
+        self.ratings = pandas.DataFrame(ratings_data).T
+        self.similarity_matrix = None
+    
+    def calculate_similarity(self):
+        self.similarity_matrix = cosine_similarity(self.ratings.fillna(0))
+        self.similarity_matrix = pandas.DataFrame(self.similarity_matrix,index=self.ratings.index,columns=self.ratings.index)
+    
+    def get_recommendations(self, user, n_recommendations=2):        
+        if self.similarity_matrix is None:
+            self.calculate_similarity()
 
-# Sample user-item rating matrix
-data = {
-    'User1': [5, 4, 0, 0, 3],
-    'User2': [4, 0, 0, 2, 4],
-    'User3': [0, 0, 5, 3, 0],
-    'User4': [0, 2, 4, 0, 0],
-    'User5': [1, 0, 0, 5, 4]
-}
-items = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5']
-ratings_matrix = pd.DataFrame(data, index=items).T
+        # Get similar users
+        similar_users = self.similarity_matrix[user].sort_values(ascending=False)
 
-# Function to calculate recommendations
-def get_recommendations(user, ratings, n_recommendations=2):
-    # Compute cosine similarity matrix
-    user_similarity = cosine_similarity(ratings.fillna(0))
-    sim_df = pd.DataFrame(user_similarity, index=ratings.index, columns=ratings.index)
+        # Gather items from similar users
+        recommendations = pandas.Series(dtype=float)
+        for similar_user in similar_users.index[1:]:  # Skip the user themselves
+            user_ratings = self.ratings.loc[similar_user]
+            recommendations = pandas.concat([recommendations, user_ratings[user_ratings > 0]])
 
-    # Get the user's similar users
-    similar_users = sim_df[user].sort_values(ascending=False)
+        # Remove items already rated by the user
+        recommendations = recommendations[~recommendations.index.isin(
+            self.ratings.loc[user][self.ratings.loc[user] > 0].index)]
 
-    # Get the items rated by similar users
-    recommendations = pd.Series(dtype=float)
-
-    for similar_user in similar_users.index[1:]:  # Skip the user themselves
-        user_ratings = ratings.loc[similar_user]
-        if recommendations.empty:
-            recommendations = user_ratings[user_ratings > 0]
-        else:
-            recommendations = pd.concat([recommendations, user_ratings[user_ratings > 0]])
-
-    # Remove items already rated by the user
-    recommendations = recommendations[~recommendations.index.isin(ratings.loc[user][ratings.loc[user] > 0].index)]
-
-    # Return the top n recommendations
-    return recommendations.sort_values(ascending=False).head(n_recommendations)
-
-# Example usage
-user_to_recommend = 'User1'
-recommended_items = get_recommendations(user_to_recommend, ratings_matrix)
-print(recommended_items)
+        return recommendations.sort_values(ascending=False).head(n_recommendations)
